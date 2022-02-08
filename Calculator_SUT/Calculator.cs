ee83d1d9f41f576b37153d1a08fb9963a0f8a348
HedonisticOpportunist <fujitahibaraki@gmail.com>
2022-02-08 00:16:39 +0000

    Add files via upload

---
+++ Calculator_SUT/Calculator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+// Adapted from * 
+// https://docs.specflow.org/projects/
+// getting-started/en/latest/GettingStarted/Step2.html
+namespace CalculatorSUT
+{
+    public class Calculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+
+        public int Add()
+        {
+            return FirstNumber + SecondNumber;
+        }
+
+        public int Divide()
+        {
+            return FirstNumber / SecondNumber;
+        }
+
+        public int Subtract()
+        {
+            return FirstNumber - SecondNumber;
+        }
+
+        public int Mulitiply()
+        {
+            return FirstNumber * SecondNumber;
+        }
+
+        public int Modulo()
+        {
+            return FirstNumber % SecondNumber;
+        }
+
+        // @ Credit: https://stackoverflow.com/questions/
+        // 3034604/is-there-an-exponent-operator-in-c/3034607
+        public double Exponential()
+        {
+            return Math.Pow(FirstNumber, SecondNumber);
+        }
+    }
+}
